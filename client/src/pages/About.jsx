1992befaccc1630602af3f27388f84bec0ff0c8f
SaipavanKatta28 <katta.sai@northeastern.edu>
2024-04-18 11:29:48 -0400

    about  file changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -24,7 +24,7 @@ export default function About() {
       Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
       </p>
       <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
-      <img src="https://pixabay.com/photos/window-house-inside-wood-luxury-3178666/" alt="Real Estate Property" className="mt-4 w-full h-auto" />
+      <img src="client\src\pages\images\house-1836070_1280.jpg" alt="Real Estate Property" className="mt-4 w-full h-auto" />
     </div>
   )
 }
